2dad3b8e2eb509dcd896538cf6bad2611376f954
Otávio Escalante <otavioescalantedev@outlookm.com>
2022-08-15 22:02:32 -0300

    project finished

---
+++ README.md
@@ -0,0 +1,15 @@
+# Project to register tasks 🚀
+
+### Description
+
+in this project it is possible to add a task, edit it, mark it as completed and delete ✅
+
+<br><br>
+<strong>Technologies used:</strong>
+
+![html](https://user-images.githubusercontent.com/77994942/184761872-1a04a223-19ec-441b-b8be-8273ed36651d.png) ![css](https://user-images.githubusercontent.com/77994942/184762875-d307e205-6c98-45b9-a520-be28b5eaefe1.png) ![js](https://user-images.githubusercontent.com/77994942/184764466-f65743c4-2ed9-431e-b49c-bc56b734a5f0.png)
+
+<br>
+   
+### Demo
+![Peek 15-08-2022 21-43](https://user-images.githubusercontent.com/77994942/184766451-241c2637-939a-4316-82cc-da02f4a1bb3b.gif)
--- scripts.js
+++ scripts.js
@@ -5,13 +5,18 @@ form.addEventListener("submit", function formInput(e) {
   const input = document.querySelector("input[name='inputTask']");
   const taskContainer = document.querySelector(".tasksContainer");
 
-  taskContainer.insertAdjacentHTML(
-    "afterbegin",
-    `<div><li> <span contentEditable>${input.value}</span>
-    <i class="fa-solid fa-trash-can"></i>
-   </li></div>`
-  );
-
+  if (input.value == "") {
+    return alert("Campo deve ser preenchido para terefa ser cadastrada");
+  }
+  if (input.value != "") {
+    taskContainer.insertAdjacentHTML(
+      "afterbegin",
+      `<div><li><input class="checkbox" type="checkbox" /> <span contentEditable>${input.value}</span>
+      <i class="fa-solid fa-trash-can"></i>
+     </li></div>`
+    );
+  }
+  input.value = "";
   const deleteBtn = document.querySelector(".fa-trash-can");
   deleteBtn.addEventListener("click", function (e) {
     const deleted = e.target;
